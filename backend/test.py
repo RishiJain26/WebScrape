0b892ec94b7b4d701a5cea6ad14e457e477db187
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-20 20:13:53 +0530

    upd test

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
-from main import main
+from main import app
 
-client = TestClient(main)
+client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
