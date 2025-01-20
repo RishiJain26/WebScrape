727a42f0c7e613a01d316df244cc56f2aee17251
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-20 22:19:47 +0530

    Upd Main

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
 import requests
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 
 @app.get("/")
 def main():
-    r = requests.get("https://www.geeksforgeeks.org/python-web-scraping-tutorial/")
-    return r.content
+    # r = requests.get("https://www.geeksforgeeks.org/python-web-scraping-tutorial/")
+
+    # return r.content
+    return "Hello World"
 
 if __name__ == "__main__":
     import uvicorn
