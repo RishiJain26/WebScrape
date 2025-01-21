767c7164775a1957c6f24828229a04a15a4de4da
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-21 20:35:31 +0530

    Update Yml file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState(''); // State to store fetched data
-  const [loading, setLoading] = useState(true); // State to handle loading
+  const [data, setData] = useState(''); 
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
-    // Function to fetch data
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:8000/');
@@ -21,15 +20,15 @@ function App() {
     };
 
     fetchData();
-  }, []); // Empty dependency array to ensure the effect runs only once
+  }, []); 
 
   return (
     <div className="App">
       <p>This is a demo</p>
       {loading ? (
-        <p>Loading...</p> // Show a loading message while the request is in progress
+        <p>Loading...</p> 
       ) : (
-        <p>{data}</p> // Show the actual fetched data
+        <p>{data}</p> 
       )}
     </div>
   );
